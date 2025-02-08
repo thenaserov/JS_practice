b9dd1d946739f10efad57c849da854aac1459f09
Naser Rezayi <54706692+thenaserov@users.noreply.github.com>
2025-02-08 10:30:40 +0330

    Create script.js

---
+++ 0004_JS_Functions/script.js
@@ -0,0 +1,14 @@
+// Function to compute the product of p1 and p2
+function myFunction(p1, p2) {
+  return p1 * p2;
+}
+
+// arrow function ES6
+let myFunction = (a, b) => a * b;
+
+hello = () => {
+  return "Hello World!";
+}
+
+// with params
+hello = (val) => "Hello " + val;
